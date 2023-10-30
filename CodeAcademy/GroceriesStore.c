61f75d7cff87b245e2814fd9d4dfbc12a336a6c8
Matteo019 <123021995+Matteo019@users.noreply.github.com>
2023-10-30 20:21:42 +0100

    Arrumei tudowwww
    Depois faço a limpa doq nao nuso maissssss.

--- CodeAcademy/GroceriesStore.c
+++ CodeAcademy/GroceriesStore.c
@@ -3,14 +3,19 @@
 int main() {
   float applePrice = 1.49;
   int appleQuantity = 23;
-  char appleLocation = 'F';
-  float appleReview = 82.5;
-  int appleReviewDisplay = 82;
-  const char APPLELOCATION = 'F';
-  appleReview = (int)appleReviewDisplay;
+  char appleLocation = 'C';
+  appleLocation = 'F';
+
+
+  double appleReview;
+  int appleReviewDisplay;
+  appleReview = 82.5;
+  appleReviewDisplay = appleReview;
+
 
  //Why it's working??????
- 
+ //Because i fixed it ;)
+
 printf("An apple costs: $%.2f, there are %d in inventory found in section: %c and your customers gave it an average review of %d%%", applePrice, appleQuantity, appleLocation, appleReviewDisplay);
 
 }
--- CodeAcademy/HelloWorld.c
+++ CodeAcademy/HelloWorld.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
 
 int main() {
+  
+ int appleQuantity;
+ double applePrice = 1.49;
+ double appleReview;
+ int appleReviewDisplay;
+ const char appleLocation = 'F';
 
-    int age = 18;
-    char favoriteLetter ='C';
+ appleQuantity = 23;
+ appleReview = 82.5;
+ 
+//New appleReview String Definition
+ int appleReviewScore = 823;
+ int appleReviewPeople = 9;
 
-    printf("Hi, my name is Matteo, i am %d years old \n and my favorite letter is %c", age, favoriteLetter);
+ //New Customer score
+ appleReviewScore += 53;
+ appleReviewPeople++;
 
-    }
+//New appleReview Value Definition
+appleReview = appleReviewScore / appleReviewPeople;
+  appleReviewDisplay = appleReview;
+
+int dayOfWeek = 0;
+dayOfWeek = 3;
+
+ if(appleQuantity < 10 || dayOfWeek == 3){
+  printf("Sale on apples today, today only they are: $%.2f\n\n", applePrice * .9);
+ }
+
+
+ // Put all your code above this and if you declare your variables using the given names and types there is no need to change any of the code below.
+ printf("An apple costs: $%.2f, there are %d in inventory found in section: %c and your customers gave it an average review of %d%%!", applePrice, appleQuantity, appleLocation, appleReviewDisplay);
+
+}
