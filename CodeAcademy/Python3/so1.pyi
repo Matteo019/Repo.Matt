65ebdc2dcfa1226e301755623f45ac988a5c471a
Matteo019 <123021995+Matteo019@users.noreply.github.com>
2023-12-18 22:21:52 +0100

    some def

---
+++ CodeAcademy/Python3/so1.pyi
@@ -0,0 +1,81 @@
+exo_kill = 2
+exo_nonkill = exo_kill
+exo_nonkill ** exo_kill
+print(exo_nonkill)
+print("""Some Basic Concepts:
+
+# comments
+In PYTHON the code already stars at a new line, so you need to be carefull cuz sometimes you want to have 2 print on the same line.
+
+print("") or even print('')
+
+string = "example1"
+
+print(string)
+We can update strings just like C.
+
+string = "example1"
+print
+string = "example2"
+print
+
+Difference between the errors you coul make:
+
+SyntaxError: Something wrong with the way your program is written.
+
+NameError: Occurs when the Code contains a word undefined.
+
+Numbers:
+
+Numeric Data Types in python are automatically refres the type of the number "if its int, float etc..."
+
+Calculations:
+
+The calculations in python are used in a really flexible way, for example:
+
+We can just do it, like print(234 - 30 / 2)
+
+You can just print a number without assigning it to a variable..
+
+Changing Numbers:
+
+It works just like C.. (the same way we change strings value)
+
+Exponents:
+
+We don't need to use the "for" function in python to calcule an exponent, we can use ** instead
+
+Example:
+
+Print(2 ** 4)
+
+Your_mama_pounds = 4 ** 12
+
+Modulo:
+
+Just like C
+
+4 % 3 = 1
+
+Concatenation:
+
+The + operator CAN ADD STRINGS TOGHETERRRRRRRRRR
+Example:
+
+message = string1 + string2
+
+Plus Equals:
+
+Shorthad +=, *=, /= (just like c)
+
+Multi-line Strings:
+
+We can use 3" 
+Text = 3" random text
+
+
+
+
+
+
+I hate to go to church """)
